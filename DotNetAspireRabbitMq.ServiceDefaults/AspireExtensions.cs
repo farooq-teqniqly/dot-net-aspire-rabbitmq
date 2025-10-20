fdedb4ba400307ecbc163f722fad9a1742918f8f
farooq-teqniqly <farooq@teqniqly.com>
2025-10-20 15:03:30 -0700

    chore: Add AspireExtensions class for service defaults, health checks, and OpenTelemetry configuration

--- DotNetAspireRabbitMq.ServiceDefaults/AspireExtensions.cs
+++ DotNetAspireRabbitMq.ServiceDefaults/AspireExtensions.cs
@@ -13,7 +13,7 @@ namespace DotNetAspireRabbitMq.ServiceDefaults;
 // Adds common .NET Aspire services: service discovery, resilience, health checks, and OpenTelemetry.
 // This project should be referenced by each service project in your solution.
 // To learn more about using this project, see https://aka.ms/dotnet/aspire/service-defaults
-public static class Extensions
+public static class AspireExtensions
 {
   private const string _alivenessEndpointPath = "/alive";
   private const string _healthEndpointPath = "/health";
